9c8a34565a682c742f5f845e0e2bde3170c309ac
Elenterius <elenterius@users.noreply.github.com>
2020-08-16 22:24:14 +0200

    adds slow down of living entities for corn crop (corn mazes anyone?)

--- src/main/java/enemeez/simplefarming/block/growable/DoubleCropBlock.java
+++ src/main/java/enemeez/simplefarming/block/growable/DoubleCropBlock.java
@@ -1,16 +1,19 @@
 package enemeez.simplefarming.block.growable;
 
-import net.minecraft.block.*;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.state.EnumProperty;
-import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.state.properties.DoubleBlockHalf;
-import net.minecraft.util.IItemProvider;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
@@ -24,97 +27,111 @@ import java.util.function.Supplier;
 /**
  * Modified by Elenterius on 15.08.2020
  */
-public class DoubleCropBlock extends SimpleCropBlock {
-	public static final EnumProperty<DoubleBlockHalf> HALF = BlockStateProperties.DOUBLE_BLOCK_HALF;
+public class DoubleCropBlock extends SimpleCropBlock
+{
+    public static final EnumProperty<DoubleBlockHalf> HALF = BlockStateProperties.DOUBLE_BLOCK_HALF;
+    private final boolean slowsMovement;
 
-	public DoubleCropBlock(Block.Properties properties, Supplier<Item> itemSupplier, Supplier<Item> seedItemSupplier) {
-		super(properties, itemSupplier, seedItemSupplier);
-		setDefaultState(stateContainer.getBaseState().with(getAgeProperty(), 0).with(HALF, DoubleBlockHalf.LOWER));
-	}
+    public DoubleCropBlock(Block.Properties properties, Supplier<Item> itemSupplier, Supplier<Item> seedItemSupplier) {
+        this(properties, itemSupplier, seedItemSupplier, false);
+    }
 
-	@Override
-	protected boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
-		return state.getBlock() instanceof FarmlandBlock;
-	}
+    public DoubleCropBlock(Block.Properties properties, Supplier<Item> itemSupplier, Supplier<Item> seedItemSupplier, boolean slowsMovement) {
+        super(properties, itemSupplier, seedItemSupplier);
+        setDefaultState(stateContainer.getBaseState().with(getAgeProperty(), 0).with(HALF, DoubleBlockHalf.LOWER));
+        this.slowsMovement = slowsMovement;
+    }
 
-	public DoubleBlockHalf getHalf(BlockState state) {
-		return state.get(HALF);
-	}
+    @Override
+    protected boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return state.getBlock() instanceof FarmlandBlock;
+    }
 
-	// Tick function
-	@Override
-	public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
-		if (!worldIn.isAreaLoaded(pos, 1))
-			return;
-		if (worldIn.getLightSubtracted(pos, 0) >= 9) {
-			int age = getAge(state);
-			if (age < getMaxAge()) {
-				float f = getGrowthChance(this, worldIn, pos);
-				if (ForgeHooks.onCropsGrowPre(worldIn, pos, state, random.nextInt((int) (25.0F / f) + 1) == 0)) {
-					if (age == getMaxAge() - 1) {
-						if (worldIn.getBlockState(pos.up()) == Blocks.AIR.getDefaultState() && worldIn.getBlockState(pos.down()).getBlock() instanceof FarmlandBlock) {
-							worldIn.setBlockState(pos.up(), this.withAge(getMaxAge()).with(HALF, DoubleBlockHalf.UPPER), Constants.BlockFlags.BLOCK_UPDATE);
-							worldIn.setBlockState(pos, this.withAge(getMaxAge()).with(HALF, DoubleBlockHalf.LOWER), Constants.BlockFlags.BLOCK_UPDATE);
-							ForgeHooks.onCropsGrowPost(worldIn, pos, state);
-						}
-					} else {
-						worldIn.setBlockState(pos, this.withAge(age + 1), Constants.BlockFlags.BLOCK_UPDATE);
-					}
-					ForgeHooks.onCropsGrowPost(worldIn, pos, state);
-				}
-			}
-		}
+    public DoubleBlockHalf getHalf(BlockState state) {
+        return state.get(HALF);
+    }
 
-	}
+    // Tick function
+    @Override
+    public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
+        if (!worldIn.isAreaLoaded(pos, 1))
+            return;
+        if (worldIn.getLightSubtracted(pos, 0) >= 9) {
+            int age = getAge(state);
+            if (age < getMaxAge()) {
+                float f = getGrowthChance(this, worldIn, pos);
+                if (ForgeHooks.onCropsGrowPre(worldIn, pos, state, random.nextInt((int) (25.0F / f) + 1) == 0)) {
+                    if (age == getMaxAge() - 1) {
+                        if (worldIn.getBlockState(pos.up()) == Blocks.AIR.getDefaultState() && worldIn.getBlockState(pos.down()).getBlock() instanceof FarmlandBlock) {
+                            worldIn.setBlockState(pos.up(), this.withAge(getMaxAge()).with(HALF, DoubleBlockHalf.UPPER), Constants.BlockFlags.BLOCK_UPDATE);
+                            worldIn.setBlockState(pos, this.withAge(getMaxAge()).with(HALF, DoubleBlockHalf.LOWER), Constants.BlockFlags.BLOCK_UPDATE);
+                            ForgeHooks.onCropsGrowPost(worldIn, pos, state);
+                        }
+                    }
+                    else {
+                        worldIn.setBlockState(pos, this.withAge(age + 1), Constants.BlockFlags.BLOCK_UPDATE);
+                    }
+                    ForgeHooks.onCropsGrowPost(worldIn, pos, state);
+                }
+            }
+        }
 
-	@Override
-	public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
-		return (worldIn.getLightSubtracted(pos, 0) >= 8 || worldIn.canSeeSky(pos)) && placementChecker(state, worldIn, pos);
-	}
+    }
 
-	private boolean placementChecker(BlockState state, IWorldReader worldIn, BlockPos pos) {
-		BlockState testState = worldIn.getBlockState(pos.down());
-		if (testState.getBlock() instanceof FarmlandBlock)
-			return true;
-		return testState == this.withAge(getMaxAge()).with(HALF, DoubleBlockHalf.LOWER) && worldIn.getBlockState(pos.down(2)).getBlock() instanceof FarmlandBlock;
-	}
+    @Override
+    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+        return (worldIn.getLightSubtracted(pos, 0) >= 8 || worldIn.canSeeSky(pos)) && placementChecker(state, worldIn, pos);
+    }
 
-	@Override
-	public boolean canGrow(IBlockReader worldIn, BlockPos pos, BlockState state, boolean isClient) {
-		return !isMaxAge(state) && worldIn.getBlockState(pos.up()) == Blocks.AIR.getDefaultState() && worldIn.getBlockState(pos.down()).getBlock() instanceof FarmlandBlock;
-	}
+    private boolean placementChecker(BlockState state, IWorldReader worldIn, BlockPos pos) {
+        BlockState testState = worldIn.getBlockState(pos.down());
+        if (testState.getBlock() instanceof FarmlandBlock)
+            return true;
+        return testState == this.withAge(getMaxAge()).with(HALF, DoubleBlockHalf.LOWER) && worldIn.getBlockState(pos.down(2)).getBlock() instanceof FarmlandBlock;
+    }
 
-	@Override
-	public void grow(World worldIn, BlockPos pos, BlockState state) {
-		int newAge = getAge(state) + getBonemealAgeIncrease(worldIn);
-		int maxAge = getMaxAge() - 1;
-		if (newAge >= maxAge && worldIn.getBlockState(pos.up()) == Blocks.AIR.getDefaultState() && worldIn.getBlockState(pos.down()).getBlock() instanceof FarmlandBlock) {
-			worldIn.setBlockState(pos, withAge(getMaxAge()), Constants.BlockFlags.BLOCK_UPDATE);
-			worldIn.setBlockState(pos.up(), withAge(getMaxAge()).with(DoubleCropBlock.HALF, DoubleBlockHalf.UPPER), Constants.BlockFlags.BLOCK_UPDATE);
-			return;
-		}
+    @Override
+    public boolean canGrow(IBlockReader worldIn, BlockPos pos, BlockState state, boolean isClient) {
+        return !isMaxAge(state) && worldIn.getBlockState(pos.up()) == Blocks.AIR.getDefaultState() && worldIn.getBlockState(pos.down()).getBlock() instanceof FarmlandBlock;
+    }
 
-		worldIn.setBlockState(pos, withAge(newAge), Constants.BlockFlags.BLOCK_UPDATE);
-	}
+    @Override
+    public void grow(World worldIn, BlockPos pos, BlockState state) {
+        int newAge = getAge(state) + getBonemealAgeIncrease(worldIn);
+        int maxAge = getMaxAge() - 1;
+        if (newAge >= maxAge && worldIn.getBlockState(pos.up()) == Blocks.AIR.getDefaultState() && worldIn.getBlockState(pos.down()).getBlock() instanceof FarmlandBlock) {
+            worldIn.setBlockState(pos, withAge(getMaxAge()), Constants.BlockFlags.BLOCK_UPDATE);
+            worldIn.setBlockState(pos.up(), withAge(getMaxAge()).with(DoubleCropBlock.HALF, DoubleBlockHalf.UPPER), Constants.BlockFlags.BLOCK_UPDATE);
+            return;
+        }
 
-	@Override
-	public void onBlockHarvested(World worldIn, BlockPos pos, BlockState state, PlayerEntity player) {
-		if (!worldIn.isRemote) {
-			Block below = worldIn.getBlockState(pos.down()).getBlock();
-			if (state.getBlock() == this && below == this) { //make sure the block below is also a double crop block
-				DoubleCropBlock crop = (DoubleCropBlock) worldIn.getBlockState(pos).getBlock();
-				if (crop.getAge(state) == getMaxAge() && crop.getHalf(state) == DoubleBlockHalf.UPPER) {
-					worldIn.setBlockState(pos.down(), crop.getDefaultState(), Constants.BlockFlags.BLOCK_UPDATE);
-				}
-			}
-		}
+        worldIn.setBlockState(pos, withAge(newAge), Constants.BlockFlags.BLOCK_UPDATE);
+    }
 
-		super.onBlockHarvested(worldIn, pos, state, player);
-	}
+    @Override
+    public void onBlockHarvested(World worldIn, BlockPos pos, BlockState state, PlayerEntity player) {
+        if (!worldIn.isRemote) {
+            Block below = worldIn.getBlockState(pos.down()).getBlock();
+            if (state.getBlock() == this && below == this) { //make sure the block below is also a double crop block
+                DoubleCropBlock crop = (DoubleCropBlock) worldIn.getBlockState(pos).getBlock();
+                if (crop.getAge(state) == getMaxAge() && crop.getHalf(state) == DoubleBlockHalf.UPPER) {
+                    worldIn.setBlockState(pos.down(), crop.getDefaultState(), Constants.BlockFlags.BLOCK_UPDATE);
+                }
+            }
+        }
 
-	@Override
-	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-		builder.add(AGE, HALF);
-	}
+        super.onBlockHarvested(worldIn, pos, state, player);
+    }
 
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(AGE, HALF);
+    }
+
+    @Override
+    public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+        if (slowsMovement && entityIn instanceof LivingEntity) {
+            entityIn.setMotionMultiplier(state, new Vec3d(0.8D, 0.75D, 0.8D));
+        }
+    }
 }
